6844a31b80424183db3ad1398df3003d578ac55e
kroberts <cppwriter@yahoo.com>
2018-12-09 15:04:43 -0800

    added day 2

---
+++ 2-2.py
@@ -0,0 +1,18 @@
+import re
+
+pat = re.compile(r'^(\d+)x(\d+)x(\d+)$')
+
+tot = 0
+with open('2.txt' ) as file:
+    for line in (l.strip() for l in file):
+        m = pat.match(line)
+        l = int(m.group(1))
+        w = int(m.group(2))
+        h = int(m.group(3))
+
+        dims = [2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l]
+        perim = min(dims)
+
+        tot += perim + (l * w * h)
+
+print(tot)
