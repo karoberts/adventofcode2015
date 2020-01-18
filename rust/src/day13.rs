e9824964093200952982a96a4bf738b230b1fe49
Keith Roberts <keith@ntersol.com>
2020-01-17 20:53:12 -0800

    about 3x faster

--- rust/src/day13.rs
+++ rust/src/day13.rs
@@ -5,39 +5,33 @@ use super::utils;
 type MappingType = utils::HashMapFnv<String, utils::HashMapFnv<String, i32>>;
 type TosType = utils::HashSetFnv<String>;
 
-fn mapval(mapping:&MappingType, to:&String, fr:&String) -> i32 {
-    *mapping.get(to).unwrap().get(fr).unwrap_or(&0)
-}
-
-fn tryit(fr:&String, tos:&TosType, mapping:&MappingType, order:&Vec<String>, cost:i32) -> i32
+fn tryit(fr:&String, tos:&mut TosType, mapping:&MappingType, order:&mut Vec<String>, cost:i32) -> i32
 {
     let mut max_cs : Option<i32> = None;
-    let mut tosp : TosType = fastset!();
-    let mut orderp : Vec<String>;
 
     for (to, m) in mapping.get(fr).unwrap() {
         if tos.contains(to) {
             continue;
         }
-        tosp.clear();
-        for v in tos.iter() {
-            tosp.insert(v.clone());
-        }
-        tosp.insert(to.clone());
-        orderp = order.clone();
-        orderp.push(to.clone());
+        tos.insert(to.clone());
+        order.push(to.clone());
 
-        let r = tryit(&to, &tosp, mapping, &orderp, m + mapval(mapping, to, fr));
+        let mapc = *mapping.get(to).unwrap().get(fr).unwrap_or(&0);
+        let r = tryit(&to, tos, mapping, order, m + mapc);
         match max_cs {
             None => max_cs = Some(r),
             Some(v) => if r > v { max_cs = Some(r) }
         };
+        tos.remove(to);
+        order.pop();
     }
 
     match max_cs {
-        None => cost + 
-            mapval(mapping, order.first().unwrap(), order.last().unwrap()) + 
-            mapval(mapping, order.last().unwrap(), order.first().unwrap()),
+        None => {
+            let mapc1 = *mapping.get(order.first().unwrap()).unwrap().get(order.last().unwrap()).unwrap_or(&0);
+            let mapc2 = *mapping.get(order.last().unwrap()).unwrap().get(order.first().unwrap()).unwrap_or(&0);
+            cost + mapc1 + mapc2
+        },
         Some(v) => v + cost
     }
 }
@@ -63,12 +57,15 @@ pub fn _run()
     }
 
     let mut highest : i32 = std::i32::MIN;
+    let mut tos : TosType = fastset!();
+    let mut order : Vec<String> = Vec::new();
 
     for fr in mapping.keys() {
-        let mut tos : TosType = fastset!();
+        tos.clear();
         tos.insert(fr.clone());
-        let order = vec![fr.clone()];
-        let c = tryit(&fr, &tos, &mapping, &order, 0);
+        order.clear();
+        order.push(fr.clone());
+        let c = tryit(&fr, &mut tos, &mapping, &mut order, 0);
         if c > highest {
             highest = c;
         }
@@ -87,10 +84,11 @@ pub fn _run()
     highest = std::i32::MIN;
 
     for fr in mapping.keys() {
-        let mut tos : TosType = fastset!();
+        tos.clear();
         tos.insert(fr.clone());
-        let order = vec![fr.clone()];
-        let c = tryit(&fr, &tos, &mapping, &order, 0);
+        order.clear();
+        order.push(fr.clone());
+        let c = tryit(&fr, &mut tos, &mapping, &mut order, 0);
         if c > highest {
             highest = c;
         }
